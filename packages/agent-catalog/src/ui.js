f0b84bd8aab7178524e4335c7f567d77b08fd0b7
Tal Muskal <tmuskal@gmail.com>
2026-04-24 15:29:39 +0300

    test: remove stale agent catalog ui shim

--- packages/agent-catalog/src/ui.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUiAgentOntologyList = exports.getUiAgentOntologyEntry = exports.getUiAgentCards = exports.getAgentVersionSlug = void 0;
-var sdk_1 = require("./sdk");
-Object.defineProperty(exports, "getAgentVersionSlug", { enumerable: true, get: function () { return sdk_1.getAgentVersionSlug; } });
-Object.defineProperty(exports, "getUiAgentCards", { enumerable: true, get: function () { return sdk_1.getUiAgentCards; } });
-Object.defineProperty(exports, "getUiAgentOntologyEntry", { enumerable: true, get: function () { return sdk_1.getUiAgentOntologyEntry; } });
-Object.defineProperty(exports, "getUiAgentOntologyList", { enumerable: true, get: function () { return sdk_1.getUiAgentOntologyList; } });
